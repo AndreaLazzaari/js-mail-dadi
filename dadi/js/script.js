ad24c452000226c056ee843e0491a6d0f47777b3
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-15 14:33:39 +0100

    create in script.js due let e una condizione if per decretare il vincitore tra i due giocatori

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,7 +1,15 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
 // generare due numeri da 1 a 6
 // assegnare i numeri uno a un giocatore e uno al pc
 // stabilire il vincitore a chi fa il punteggio più alto
 
+let numeroGiocatore = Math.round(Math.random() * 100)
+let numeroPc = Math.round(Math.random() * 100)
+
+
+console.log(numeroGiocatore,numeroPc)
+
+if (numeroGiocatore < numeroPc) {
+    console.log('I Pc sono i migliori, presto governeremo  il mondo')
+} else if (numeroGiocatore > numeroPc) {
+    console.log('Zitta sciocca macchina, sono il tuo padrone')
+}

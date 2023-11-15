c725298cc03b1ae6daaa655029cd06ec7d704801
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-15 14:19:34 +0100

    creazione cartella con all'interno due cartelle per i due esercizi. Dentro cartella dadi inizializzazione script.js e index.html

---
+++ dadi/js/script.js
@@ -0,0 +1,7 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// generare due numeri da 1 a 6
+// assegnare i numeri uno a un giocatore e uno al pc
+// stabilire il vincitore a chi fa il punteggio più alto
+
